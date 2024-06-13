177a6839b677c7bfab4d7cf4afb9e8929ba8acc4
AMANDA <amandawahyu@gmail.com>
2024-06-13 20:40:45 +0700

    product-add-API

--- bazarkomplek/src/app/api/products/add/route.ts
+++ bazarkomplek/src/app/api/products/add/route.ts
@@ -4,10 +4,10 @@ export const dynamic = "force-dynamic";
 export async function POST(request: Request) {
     try {
         const body = await request.json();
-        let newProductBody = _.pick(body,["name", "slug", "images", "description", "excerpt", "type", "status", "price", "eventId"]);
+        let newProductBody = _.pick(body,["name", "slug", "images", "description", "excerpt", "type", "category", "status", "price", "eventId"]);
         const userId = request.headers.get("x-id-user") as string;
-        newProductBody = newProductBody.status ? newProductBody : { ...newProductBody, status: undefined }
-        const result = await Products.addProduct({ sellerId: userId, ...newProductBody })
+        newProductBody = newProductBody.status ? newProductBody : { ...newProductBody, status: "VERIFYING" }
+        const result = await Products.addProduct({ sellerId: userId, ...newProductBody, status: "VERIFYING" })
         return Response.json({ result });
     } catch (error) {
         console.log(error)
