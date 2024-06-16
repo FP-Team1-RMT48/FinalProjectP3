68582f86ccd5de95dae4307560df044c85cb05e9
makbara000 <makbara000@gmail.com>
2024-06-16 21:03:12 +0700

    feat: wiring for add/detail admins

--- bazarkomplek/src/app/admin-events/add-event/page.tsx
+++ bazarkomplek/src/app/admin-events/add-event/page.tsx
@@ -1,9 +1,40 @@
+"use client";
+
+import { addEvent } from "@/app/action";
+import { useState } from "react";
+
 export default function AddEventPage() {
+  const [formData, setFormData] = useState({
+    name: "",
+    location: "",
+    eventImg: "",
+    startDate: "",
+    endDate: "",
+    lapakSlots: 0,
+    filledLapakSlots: 0,
+    eventSlug: "",
+  });
+  function handleChange(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
+  function handleFormData(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    try {
+      addEvent(formData);
+    } catch (error) {}
+  }
   return (
     <main className="flex min-h-screen flex-col items-center gap-10 py-10 text-base-100">
       <h3 className="font-bold text-3xl">ADD EVENT</h3>
       <form
         action=""
+        onSubmit={handleFormData}
         className="bg-base-100 w-11/12 h-auto flex flex-col border-2 p-4 gap-4 sm:gap-6 sm:p-6  rounded-lg"
       >
         <div className="flex flex-col justify-center gap-4 lg:flex-row lg:justify-evenly">
@@ -13,6 +44,8 @@ export default function AddEventPage() {
             </label>
             <input
               type="text"
+              name="name"
+              onChange={handleChange}
               className="text-sm py-1 pl-2 w-full bg-white text-base-100 rounded-md md:text-base lg:text-lg"
             />
           </div>
@@ -24,6 +57,8 @@ export default function AddEventPage() {
             </label>
             <input
               type="date"
+              name="startDate"
+              onChange={handleChange}
               className="text-sm py-1 pl-2 w-full bg-white text-base-100 rounded-md md:text-base lg:text-lg"
             />
           </div>
@@ -33,6 +68,8 @@ export default function AddEventPage() {
             </label>
             <input
               type="date"
+              name="endDate"
+              onChange={handleChange}
               className="text-sm py-1 pl-2 w-full bg-white text-base-100 rounded-md md:text-base lg:text-lg"
             />
           </div>
@@ -43,6 +80,8 @@ export default function AddEventPage() {
           </label>
           <input
             type="text"
+            name="eventImg"
+            onChange={handleChange}
             className="text-sm py-1 pl-2 w-full bg-white text-base-100 rounded-md md:text-base lg:text-lg"
           />
         </div>
@@ -50,10 +89,17 @@ export default function AddEventPage() {
           <label htmlFor="" className="text-xs pl-2 text-white lg:text-base">
             Event location
           </label>
-          <textarea className="text-sm py-1 pl-2 w-full bg-white text-base-100 rounded-md md:text-base lg:text-lg" />
+          <textarea
+            name="location"
+            onChange={handleChange}
+            className="text-sm py-1 pl-2 w-full bg-white text-base-100 rounded-md md:text-base lg:text-lg"
+          />
         </div>
-        <button className="bg-white rounded-lg w-3/6 xs:w-2/6 self-center p-2 mt-4">
-          Edit Product
+        <button
+          type="submit"
+          className="bg-white rounded-lg w-3/6 xs:w-2/6 self-center p-2 mt-4"
+        >
+          Add Event
         </button>
       </form>
     </main>
