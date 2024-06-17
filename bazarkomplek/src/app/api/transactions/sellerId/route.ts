517c68ea2e4df55439e90a1b8ba6714e64aba7a0
AMANDA <amandawahyu@gmail.com>
2024-06-17 17:59:25 +0700

    all transaction done

--- bazarkomplek/src/app/api/transactions/sellerId/route.ts
+++ bazarkomplek/src/app/api/transactions/sellerId/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: Request) {
     console.log(request)
     try {
         const sellerId = request.headers.get('x-id-user');
-
+        console.log(sellerId, "<<sellerId")
         if (!sellerId) {
             return new Response(JSON.stringify({ message: "Unauthorized" }), { status: 401 });
         }
